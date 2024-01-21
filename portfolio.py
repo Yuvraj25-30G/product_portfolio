fee943979cca0236d9be15c5cfc84e3a916b5c55
yuvraj <yuvrajsaxena2015@gmail.com>
2024-01-22 01:55:33 +0530

    update drive link

--- portfolio.py
+++ portfolio.py
@@ -62,7 +62,7 @@ if selected == 'About':
                         </svg>
                         <span style="margin-left: 5px;">View Linkedin Profile</span>
                     </a>
-                    <a style='display: inline-flex; margin-right: 5px; align-items: center; padding: 10px 20px; background-color: #FF6252; color: white; text-decoration: none; border-radius: 5px;' href="https://drive.google.com/file/d/1-eDl0H5lEq5D7y3GKwb5YWzlpaMruOyw/view?usp=sharing" target="_blank">
+                    <a style='display: inline-flex; margin-right: 5px; align-items: center; padding: 10px 20px; background-color: #FF6252; color: white; text-decoration: none; border-radius: 5px;' href="https://drive.google.com/file/d/1-eDl0H5lEq5D7y3GKwb5YWzlpaMruOyw/view?usp=drive_link" target="_blank">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
                         <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5"/>
                         <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
